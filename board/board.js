4d5e546aa6792c44d96773b163b8d5bde520347a
Maurice Oegerli <maurice@oeger.li>
2022-01-30 16:41:12 +0100

    add new field on drag and drop

--- board/board.js
+++ board/board.js
@@ -2,8 +2,11 @@ const occupiedFields = [];
 
 export const occupyField = (piece) => {
   occupiedFields.push(piece);
-}
+};
 
 export const getOccupiedFields = () => {
   return occupiedFields;
-}
+};
+
+export const isFieldOccupied = (x, y) =>
+  occupiedFields.some((piece) => piece.posX === x && piece.posY === y);
