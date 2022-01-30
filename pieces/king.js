8ab15110cf5a93c2f923838547e5fa89bfcfce98
andr0mega <bruehwiler.colin@gmail.com>
2022-01-30 18:47:39 +0100

    king moves fixed

--- pieces/king.js
+++ pieces/king.js
@@ -1,30 +1,34 @@
-import { getOccupiedFields, renderPossibleFields } from '../board/board';
+import { getOccupiedFields } from '../board/board';
 import Piece from './piece';
 
 export default class King extends Piece {
   type = 'king';
 
   getPossibleMoves() {
-    const possibleFields = [];
+    let possibleFields = [];
     for (let x = 1; x <= 8; x++) {
       for (let y = 1; y <= 8; y++) {
         const offsetX = x - this.posX;
         const offsetY = y - this.posY;
         if (offsetX <= 1 && offsetX >= -1 && offsetY <= 1 && offsetY >= -1) {
-          getOccupiedFields().forEach((field) => {
-            if (
-              !(
-                field.posX === x &&
-                field.posY === y &&
-                field.color === this.color
-              )
-            ) {
-              possibleFields.push({ posX: x, posY: y });
-            }
-          });
+          possibleFields.push({ posX: x, posY: y });
         }
       }
     }
+    console.log(possibleFields);
+    const possibleFieldsOccupied = getOccupiedFields().filter((field) => {
+      const offsetX = field.posX - this.posX;
+      const offsetY = field.posY - this.posY;
+      return offsetX <= 1 && offsetX >= -1 && offsetY <= 1 && offsetY >= -1;
+    });
+		const possibleFieldsOccupiedColor = possibleFieldsOccupied.filter(field => {
+			return field.color === this.color;
+		})
+		possibleFieldsOccupiedColor.forEach(piece => {
+			possibleFields = possibleFields.filter(field => {
+				return !(field.posX === piece.posX && field.posY === piece.posY)
+			})
+		})
     return possibleFields;
   }
 }
